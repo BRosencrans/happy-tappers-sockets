28f440b7ce5806a93d0843670e0e2fdb67ebb6fe
Clarice Tran <tran.clarice@gmail.com>
2023-03-13 08:38:14 -0700

    moved eventlisteners and emitters to a separate file from socket server setup. Created listeners  for when a new room is created, when a room is joined, when a room is left, and requests for room data.

--- management/index.js
+++ management/index.js
@@ -1,4 +1,6 @@
 const roomManagement = require("./roomManagement");
+const userManagement = require("./userManagement");
+const memGameManagment = require("./memGameManagement");
 
 module.exports = {
     roomManagement,
